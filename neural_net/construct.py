fe9dddff5e0f7fd52f95f6142c74b5dfce657404
moon-jong <dksjfl@naver.com>
2020-11-12 22:17:11 +0900

    add calcul_error for backprop

--- neural_net/construct.py
+++ neural_net/construct.py
@@ -1,15 +1,18 @@
 import numpy as np
 
+
+# this is the class for construct a simple neural network with 2 input and 2 hidden node and 2 output
 class constructor:
-    def __init__(self,input_list, hidden_nodes, output_nodes):
+    def __init__(self, input_list, label_list, hidden_nodes, output_nodes):
         self.input_list = input_list
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
-        #initialize weight as all ones matrix with bias
-        self.weights_ih = np.random.rand(hidden_nodes, len(input_list))*2 -1
-        self.weights_ho = np.random.rand(output_nodes, hidden_nodes)*2 -1
-        self.bias_ih = np.ones((hidden_nodes,1))
-        self.bias_ho = np.ones((output_nodes,1))
+        self.label_mat = np.matrix(label_list).T
+        # initialize weight as all ones matrix with bias
+        self.weights_ih = np.random.rand(hidden_nodes, len(input_list)) * 2 - 1
+        self.weights_ho = np.random.rand(output_nodes, hidden_nodes) * 2 - 1
+        self.bias_ih = np.ones((hidden_nodes, 1))
+        self.bias_ho = np.ones((output_nodes, 1))
 
     def weight_sum_ih(self):
         input_layer = np.matrix(self.input_list).T
@@ -28,7 +31,31 @@ class constructor:
 
         return output
 
-
     def sigmoid(self, x):
 
-        return 1/(1 + np.exp(-x))
+        return 1 / (1 + np.exp(-x))
+
+    # To propagate backward, we should calculate the all error between the node
+
+    def calc_error(self):
+        error_mat = self.label_mat - self.output
+
+        # Construct the size of hidden weights to contain the portion of single weight
+        error_weight_ho = np.zeros(self.weights_ho.shape)
+
+
+        for col in range(self.weights_ho.shape[1]):
+            for row in range(self.weights_ho.shape[0]):
+                error_weight_ho[row][col] = self.weights_ho.T[row][col] / sum(self.weights_ho.T[row])
+
+        hidden_error = error_weight_ho * error_mat
+
+        error_weight_ih = np.zeros((self.weights_ih.shape))
+
+        for col in range(self.weights_ih.shape[1]):
+            for row in range(self.weights_ih.shape[0]):
+                error_weight_ih[row][col] = self.weights_ih.T[row][col] / sum(self.weights_ih.T[row])
+
+        input_error = error_weight_ih * hidden_error
+
+        return hidden_error, input_error
--- neural_net/nn.py
+++ neural_net/nn.py
@@ -1,16 +1,20 @@
 from neural_net.construct import constructor
 
 
-input_list = [1,2,3]
+input_list = [1,2]
+label_list = [0.7, 1.2]
 hidden_nodes = 2
-output_nodes = 1
+output_nodes = 2
 
-new_nn = constructor(input_list, hidden_nodes, output_nodes)
+new_nn = constructor(input_list, label_list, hidden_nodes, output_nodes)
 
 new_nn.weight_sum_ih()
 
 new_nn.weight_sum_oh()
 
-print('hidden_layer : ', new_nn.hidden_layer)
-print('output : ', new_nn.output)
+hidden_node_e, input_node_e = new_nn.calc_error()
+
+print('hidden_layer : \n', new_nn.hidden_layer,'\n')
+print(new_nn.weights_ih)
+print(hidden_node_e, input_node_e)
 
